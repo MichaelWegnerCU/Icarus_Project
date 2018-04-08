f8ba3d4a0b36d4cc25530b8e34ece596d7a43ef4
Quinlin McNatt <quinlinmcnatt@Quinlins-MacBook-Pro.local>
2018-04-08 14:07:34 -0600

    Milestone 4

---
+++ Milestones/Milestone 4/InsertQueries.sql
@@ -0,0 +1,23 @@
+INSERT INTO Student 
+(StudentID, FirstName, LastName, EmailAddress, PhoneNumber, Password)
+VALUES
+(102836048, Quinlin, McNatt, qumc8642@colorado.edu, 9702095471, ********)
+;
+
+
+INSERT INTO Teacher
+(TeacherID, FirstName, LastName, EmailAddress, PhoneNumber, Password)
+VALUES
+(12839293, Vivian, Lai, Vivian.Lai@colorado.edu, 1234567891, asdfasdf)
+;
+
+INSERT INTO Question
+(QuestionID, QuestionContent)
+VALUES
+(12384923, What is 2 + 2?)
+;
+
+INSERT INTO Class
+(ClassID, ClassName, ClassDescription)
+VALUES
+(1234123, Software_Development, A class in which you learn about developing software)
