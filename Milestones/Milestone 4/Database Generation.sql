2352de75b31ad15cc00f152a9bb5f3810dc41c53
Michael Wegner <michaelwegner@Michaels-MacBook-Pro.local>
2018-04-19 16:56:24 -0600

    Edited sql create statements to use teh proper database name

--- Milestones/Milestone 4/Database Generation.sql
+++ Milestones/Milestone 4/Database Generation.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema IcarusProject
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema IcarusProject
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `IcarusProject` DEFAULT CHARACTER SET utf8 ;
+USE `IcarusProject` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Student`
+-- Table `IcarusProject`.`Student`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Student` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`Student` (
   `StudentID` INT NOT NULL,
   `FirstName` VARCHAR(45) NULL,
   `LastName` VARCHAR(45) NULL,
@@ -29,9 +29,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Teacher`
+-- Table `IcarusProject`.`Teacher`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Teacher` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`Teacher` (
   `TeacherID` INT NOT NULL,
   `FirstName` VARCHAR(45) NULL,
   `LastName` VARCHAR(45) NULL,
@@ -43,9 +43,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Question`
+-- Table `IcarusProject`.`Question`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Question` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`Question` (
   `QuestionID` INT NOT NULL,
   `QuestionContent` VARCHAR(500) NULL,
   `Student_StudentID` INT NOT NULL,
@@ -55,21 +55,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Question` (
   INDEX `fk_Question_Teacher1_idx` (`Teacher_TeacherID` ASC),
   CONSTRAINT `fk_Question_Student1`
     FOREIGN KEY (`Student_StudentID`)
-    REFERENCES `mydb`.`Student` (`StudentID`)
+    REFERENCES `IcarusProject`.`Student` (`StudentID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Question_Teacher1`
     FOREIGN KEY (`Teacher_TeacherID`)
-    REFERENCES `mydb`.`Teacher` (`TeacherID`)
+    REFERENCES `IcarusProject`.`Teacher` (`TeacherID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Comment`
+-- Table `IcarusProject`.`Comment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Comment` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`Comment` (
   `CommentID` INT NOT NULL,
   `CommentContent` VARCHAR(500) NULL,
   `Question_QuestionID` INT NOT NULL,
@@ -77,16 +77,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Comment` (
   INDEX `fk_Comment_Question1_idx` (`Question_QuestionID` ASC),
   CONSTRAINT `fk_Comment_Question1`
     FOREIGN KEY (`Question_QuestionID`)
-    REFERENCES `mydb`.`Question` (`QuestionID`)
+    REFERENCES `IcarusProject`.`Question` (`QuestionID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`School`
+-- Table `IcarusProject`.`School`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`School` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`School` (
   `SchoolID` INT NOT NULL,
   `SchoolName` VARCHAR(45) NULL,
   PRIMARY KEY (`SchoolID`))
@@ -94,9 +94,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Department`
+-- Table `IcarusProject`.`Department`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Department` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`Department` (
   `DepartmentID` INT NOT NULL,
   `DirectorName` VARCHAR(45) NULL,
   `DepartmentName` VARCHAR(45) NULL,
@@ -106,16 +106,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Department` (
   INDEX `fk_Department_School_idx` (`School_SchoolID` ASC),
   CONSTRAINT `fk_Department_School`
     FOREIGN KEY (`School_SchoolID`)
-    REFERENCES `mydb`.`School` (`SchoolID`)
+    REFERENCES `IcarusProject`.`School` (`SchoolID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Class`
+-- Table `IcarusProject`.`Class`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Class` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`Class` (
   `ClassID` INT NOT NULL,
   `ClassName` VARCHAR(45) NULL,
   `ClassDescription` VARCHAR(45) NULL,
@@ -128,47 +128,47 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Class` (
   INDEX `fk_Class_Student1_idx` (`Student_StudentID` ASC),
   CONSTRAINT `fk_Class_Department1`
     FOREIGN KEY (`Department_DepartmentID`)
-    REFERENCES `mydb`.`Department` (`DepartmentID`)
+    REFERENCES `IcarusProject`.`Department` (`DepartmentID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Class_Teacher1`
     FOREIGN KEY (`Teacher_TeacherID`)
-    REFERENCES `mydb`.`Teacher` (`TeacherID`)
+    REFERENCES `IcarusProject`.`Teacher` (`TeacherID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Class_Student1`
     FOREIGN KEY (`Student_StudentID`)
-    REFERENCES `mydb`.`Student` (`StudentID`)
+    REFERENCES `IcarusProject`.`Student` (`StudentID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`StudentTeacher`
+-- Table `IcarusProject`.`StudentTeacher`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`StudentTeacher` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`StudentTeacher` (
   `Teacher_TeacherID` INT NOT NULL,
   `Student_StudentID` INT NOT NULL,
   INDEX `fk_StudentTeacher_Teacher1_idx` (`Teacher_TeacherID` ASC),
   INDEX `fk_StudentTeacher_Student1_idx` (`Student_StudentID` ASC),
   CONSTRAINT `fk_StudentTeacher_Teacher1`
     FOREIGN KEY (`Teacher_TeacherID`)
-    REFERENCES `mydb`.`Teacher` (`TeacherID`)
+    REFERENCES `IcarusProject`.`Teacher` (`TeacherID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_StudentTeacher_Student1`
     FOREIGN KEY (`Student_StudentID`)
-    REFERENCES `mydb`.`Student` (`StudentID`)
+    REFERENCES `IcarusProject`.`Student` (`StudentID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Answer`
+-- Table `IcarusProject`.`Answer`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Answer` (
+CREATE TABLE IF NOT EXISTS `IcarusProject`.`Answer` (
   `AnswerID` INT NOT NULL AUTO_INCREMENT,
   `AnswerText` VARCHAR(500) NULL,
   `AnswerValid` TINYINT NOT NULL,
@@ -177,7 +177,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Answer` (
   INDEX `fk_Answer_Question1_idx` (`Question_QuestionID` ASC),
   CONSTRAINT `fk_Answer_Question1`
     FOREIGN KEY (`Question_QuestionID`)
-    REFERENCES `mydb`.`Question` (`QuestionID`)
+    REFERENCES `IcarusProject`.`Question` (`QuestionID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
