fc570af4a23a6779a7b0bff674106a873d6a8430
user <aidanduggan95@gmail.com>
2018-04-29 23:11:13 -0600

    Changed slightly the login header

---
+++ Application/login_header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Icarus</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+ 
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div id="navbar1" >
+		 <ul class="navbar-nav">
+			 	<img src="Pics/logo/IcarusFeather.png" alt="Icarus Logo" width="75" height="50">
+                <li class="nav-item active">
+                    <a class="nav-link" href="user_homepage.php">Homepage </a>
+                </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="contact_us.html">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="terms_and_conditions.php">Terms and Conditions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signout.php">Sign Out</a>
+           		</li>
+            </ul>
+		  </div>
+	</nav>
+
+
+<div id="content">
+
+			
+			
+			
